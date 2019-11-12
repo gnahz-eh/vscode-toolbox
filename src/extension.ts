08fc6533c36603a0881da42a9ad78aaed368fcaa
gnahZ-eH <hershxy@sjtu.edu.cn>
2019-11-12 16:06:18 +0800

    Modify: Adjust source code to right package.

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import Parser from './Parser';
-import TableFormatter from './TableFormatter';
+import Parser from './converter/csv2table/Parser';
+import TableFormatter from './converter/csv2table/TableFormatter';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
